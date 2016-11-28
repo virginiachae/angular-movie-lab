53345da01323eb1813c71bd6c73c86281eb26ae2
virginiachae <chae.virginia@gmail.com>
2016-11-28 12:31:47 -0800

    completed ngSubmit challenge

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
 angular
-  .module('moviesApp', [])
-  .controller('MoviesController', MoviesCtrl);
+    .module('moviesApp', [])
+    .controller('MoviesController', MoviesCtrl);
 
-  function MoviesCtrl () {
+function MoviesCtrl() {
 
     var vm = this;
 
-    vm.movieList = [
-      {
-        id: 1,
-        name: 'Hacksaw Ridge',
-        genre: 'Action and Adventure'
-      },
-      {
-        id: 10,
-        name: 'Dr. Strange',
-        genre: 'Science Fiction & Fantasy'
-      },
-      {
-        id: 39,
-        name: 'Arrival',
-        genre: 'Mystery & Suspense'
-      },
-      {
-        id: 94,
-        name: 'Moana',
-        genre: 'Kids & Family'
-      },
-      {
-        id: 143,
-        name: 'Kubo and the Two Strings',
-        genre: 'Animation'
-      }
-    ];
-    console.log(vm);
-  };
+        vm.moviesToWatch = [{
+            id: 1,
+            name: 'Hacksaw Ridge',
+            genre: 'Action and Adventure'
+        }, {
+            id: 10,
+            name: 'Dr. Strange',
+            genre: 'Science Fiction & Fantasy'
+        }, {
+            id: 39,
+            name: 'Arrival',
+            genre: 'Mystery & Suspense'
+        }, {
+            id: 94,
+            name: 'Moana',
+            genre: 'Kids & Family'
+        }, {
+            id: 143,
+            name: 'Kubo and the Two Strings',
+            genre: 'Animation'
+        }];
+
+				vm.addMovie = function() {
+						var newObj = {name: vm.title}
+						vm.moviesToWatch.push(newObj);
+						  vm.text = '';
+					};
+
+        console.log(vm);
+    };
